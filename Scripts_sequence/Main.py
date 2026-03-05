95486c68576be8e041fec3b1c2a509fede86dc51
Ruth NJANTANG <njantang7@gmail.com>
2026-03-05 13:12:40 +0100

    run perfectly with the name of the protein

--- Scripts_sequence/Main.py
+++ Scripts_sequence/Main.py
@@ -1,27 +1,28 @@
 import os
-from test import search_pdb_by_sequence, filter_experimental_conditions
+from PDB_searchAPI import search_pdb_by_sequence, filter_experimental_conditions
 from plot import run_plot
 from chem_class_reagents import process_csv
 
 def main():
     sequence = input("Enter sequence: ").strip()
     seq_type = input("Enter sequence type (protein/dna/rna): ").strip().lower()
+    seq_type_name = input("Enter a descriptive sequence type name: ").strip()
 
     # Create output folder
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    output_dir = os.path.join(base_dir, "output", seq_type)
+    output_dir = os.path.join(base_dir, "output", seq_type_name)
     os.makedirs(output_dir, exist_ok=True)
 
     # Save FASTA
-    fasta_path = os.path.join(output_dir, f"{seq_type}_sequence.fasta")
+    fasta_path = os.path.join(output_dir, f"{seq_type_name}_sequence.fasta")
     with open(fasta_path, "w") as fasta_file:
-        fasta_file.write(f">{seq_type}\n")
+        fasta_file.write(f">{seq_type_name}\n")
         for i in range(0, len(sequence), 60):
             fasta_file.write(sequence[i:i+60] + "\n")
     print(f"✔ Input sequence saved as FASTA: {fasta_path}")
 
     # Run PDB search
-    full_csv = os.path.join(output_dir, f"{seq_type}_pdb_mmcif_full.csv")
+    full_csv = os.path.join(output_dir, f"{seq_type_name}_pdb_mmcif_full.csv")
     full_csv_result = search_pdb_by_sequence(sequence, seq_type, output_csv=full_csv, keep_all=True)
 
     if full_csv_result is None:
@@ -30,11 +31,11 @@ def main():
         return
 
     # Filter experimental conditions
-    filtered_csv = os.path.join(output_dir, f"{seq_type}_pdb_mmcif_filtered.csv")
+    filtered_csv = os.path.join(output_dir, f"{seq_type_name}_pdb_mmcif_filtered.csv")
     filter_experimental_conditions(full_csv, filtered_csv)
 
     # Parse reagents + plot
-    parsed_csv = os.path.join(output_dir, f"{seq_type}_reagents_parsed.csv")
+    parsed_csv = os.path.join(output_dir, f"{seq_type_name}_reagents_parsed.csv")
     json_path = os.path.join(base_dir, "Data", "reagents.json")
     process_csv(filtered_csv, parsed_csv, json_path)
 
