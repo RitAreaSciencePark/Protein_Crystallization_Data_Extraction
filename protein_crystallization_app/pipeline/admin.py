00c18323b11c26d1053ff00247ad8783dfdfc1d3
Ruth NJANTANG <njantang7@gmail.com>
2026-08-11 15:40:37 +0200

    Rename crystal_explorer(1) to crystal_explorer, remove protein_crystallization_app
    Drops the stray "(1)" from the working app's directory name and
    retires the old protein_crystallization_app folder, which had already
    been deleted locally but never committed.

--- protein_crystallization_app/pipeline/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Submission
-
-@admin.register(Submission)
-class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ('sequence_name', 'created_at')
-# Register your models here.
