f90de33c431d10c0d3d9bd6b8dd4787671d862e4
Ruth NJANTANG <njantang7@gmail.com>
2026-08-25 10:29:06 +0200

    update crystal_explorer webapp

--- crystal_explorer/viewer/admin.py
+++ crystal_explorer/viewer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SearchRun
+
+
+@admin.register(SearchRun)
+class SearchRunAdmin(admin.ModelAdmin):
+    list_display = ("protein_name", "status", "row_count", "created_at", "folder_name")
+    list_filter = ("status", "sequence_type", "llm_fallback")
+    search_fields = ("protein_name", "folder_name")
+    readonly_fields = [f.name for f in SearchRun._meta.fields]
