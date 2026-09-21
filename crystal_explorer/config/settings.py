38fdacff93dfe3d3eaa2d4d31af32ca31e084109
Ruth NJANTANG <njantang7@gmail.com>
2026-09-21 16:25:50 +0200

    Update crystal explorer

--- crystal_explorer/config/settings.py
+++ crystal_explorer/config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2u-6p@zaa7+f3r%b#7e68glz=ddh-x*sl3ut!)7f6&vfpgx*b8'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
