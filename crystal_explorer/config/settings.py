186fc877faae75e03d3a692484ed55e3af125956
Ruth NJANTANG <njantang7@gmail.com>
2026-09-22 16:00:04 +0200

    Restore Django settings

--- crystal_explorer/config/settings.py
+++ crystal_explorer/config/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-
+SECRET_KEY = 'q621%4!47ek&4a)wy6+vny_#cl(@cghb4!*qf4c)_35_9a=ja3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
